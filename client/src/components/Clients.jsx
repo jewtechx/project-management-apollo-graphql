9aea3faf378b9973842fc4be707871cfb4e1f316
jewtechx <jklarbi_danquah@st.ug.edu.gh>
2024-01-29 13:04:35 -0800

    feat(client) : set up apollo caching

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { gql,useQuery } from '@apollo/client'
+import { gql,useQuery} from '@apollo/client'
 import { GET_CLIENTS } from '../queries/clientQueries'
 import Spinner from './Spinner'
 import ClientRow from './ClientRow'
@@ -10,6 +10,7 @@ export default function Clients() {
     const {loading,error,data} = useQuery(GET_CLIENTS)
     if (loading) return <Spinner/>
     if(error) return <p>Something went wrong</p>
+
 
   return (
 
